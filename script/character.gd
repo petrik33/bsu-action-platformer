b736c0e64ff76ae6a1cc5185addce6d6e23b7d82
Timofey Petrikevich <petrik3003@gmail.com>
2026-09-24 00:05:20 +0300

    implement player controller with jump and animations

--- script/character.gd
+++ script/character.gd
@@ -1,12 +1,33 @@
 extends CharacterBody2D
 
-@export var speed = 400
-
-func get_input():
-	var input_direction := Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+@export var speed := 400
+@export var jump_velocity := 300
+@export var sprite: AnimatedSprite2D
 
 
-func _physics_process(_delta: float):
-	get_input()
+func _process(_delta: float) -> void:
+	if not is_on_floor():
+		if velocity.y <= 0:
+			sprite.animation = "jump_up"
+		else:
+			sprite.animation = "jump_down"
+	elif abs(velocity.x) > 0:
+		sprite.animation = "walk"
+	else:
+		sprite.animation = "idle"
+		
+	if abs(velocity.x) > 0:
+		sprite.flip_h = sign(velocity.x) == -1
+
+
+func _physics_process(delta: float):
+	if not is_on_floor():
+		velocity.y += 900 * delta
+	
+	var input_direction := Input.get_axis("left", "right")
+	velocity.x = input_direction * speed
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_velocity
+	
 	move_and_slide()
