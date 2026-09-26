ad0b93b6815ff550146d8418d3920107160451b0
Timofey Petrikevich <petrik3003@gmail.com>
2026-09-26 14:14:11 +0300

    Implement character attack

--- script/character.gd
+++ script/character.gd
@@ -5,9 +5,24 @@ extends CharacterBody2D
 
 @export var sprite: AnimatedSprite2D
 @export var visuals: Node2D
+@export var animation_player: AnimationPlayer
+@export var hit_box: Area2D
+
+
+var is_attacking: bool
 
 
 func _process(_delta: float) -> void:
+	if abs(velocity.x) > 0:
+		_flip(sign(velocity.x))
+	
+	if is_attacking:
+		return
+	
+	if Input.is_action_just_pressed("click"):
+		_attack()
+		return
+	
 	if not is_on_floor():
 		if velocity.y <= 0:
 			sprite.animation = "jump_up"
@@ -17,9 +32,6 @@ func _process(_delta: float) -> void:
 		sprite.animation = "run"
 	else:
 		sprite.animation = "idle"
-		
-	if abs(velocity.x) > 0:
-		visuals.scale.x = abs(visuals.scale.x) * sign(velocity.x)
 
 
 func _physics_process(delta: float):
@@ -33,3 +45,23 @@ func _physics_process(delta: float):
 		velocity.y = -jump_velocity
 	
 	move_and_slide()
+
+
+func _attack():
+	is_attacking = true
+	animation_player.play("attack")
+
+
+func _finish_attack():
+	is_attacking = false
+
+
+func _flip(sign: float):
+	visuals.scale.x = sign
+	hit_box.scale.x = sign
+	
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "attack":
+		_finish_attack()
